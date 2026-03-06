46792d281e0a79c1d301db43cfc53038f7e5d5b1
Josh Gronneberg <joshua.gronneberg@ndsu.edu>
2026-03-06 11:51:10 -0600

    Added Signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { Child } from './child/child';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Child],
+  imports: [Child, FormsModule],
   template: `
     <div class="container parent">
       <h2>Parent Component (App Component)</h2>
+      <p> Message to Child: <input type="text" [(ngModel)]="parentMessage"> </p>
       <p>Message from Child: </p>
       <div>
-        <app-child></app-child>
+        <app-child [message]="parentMessage()"/>
       </div>
     </div>
   `,
   styles: `.parent{background-color: pink; color: black}`
 })
 export class App {
+  parentMessage = signal<string>('');  
 
 }
 
--- src/app/child/child.ts
+++ src/app/child/child.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="child">
       <h3>Child Component</h3>
-      <p>Message received from parent: </p>
+      <p>Message received from parent: {{message()}}</p>
       <input type="text">
       <button>Send Message!</button>
     </div>
@@ -14,5 +14,5 @@ import { Component } from '@angular/core';
   styles: `.child{background-color: black ; color: white ; margin:2rem; padding:2rem}`
 })
 export class Child {
-
+  message = input<string>();
 }
