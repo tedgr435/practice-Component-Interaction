2988a491dd9b61f12f7e2e709ec7804341c85d92
Josh Gronneberg <joshua.gronneberg@ndsu.edu>
2026-03-16 13:27:22 -0500

    Added component interaction

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,18 +8,29 @@ import { FormsModule } from '@angular/forms';
   template: `
     <div class="container parent">
       <h2>Parent Component (App Component)</h2>
-      <p> Message to Child: <input type="text" [(ngModel)]="parentMessage"> </p>
-      <p>Message from Child: </p>
+      <!-- <p> Message to Child: <input type="text" [(ngModel)]="parentMessage"> </p> -->
+       <p> Message to Child: <input type="text" [(ngModel)]="parentMessage"> </p>
+       <p>Message from Child: {{parentMessage()}}</p>
+      <!-- <p>Message from Child: {{childMessage()}}</p> -->
       <div>
-        <app-child [message]="parentMessage()"/>
+       <app-child   [(value)] = "parentMessage"     />
+        <!-- <app-child 
+        [message]="parentMessage()"
+        (Clicked)="receivedMessage($event)"
+        /> -->
       </div>
     </div>
   `,
   styles: `.parent{background-color: pink; color: black}`
 })
 export class App {
-  parentMessage = signal<string>('');  
+  parentMessage = signal<string>(''); 
+  // parentMessage = signal<string>(''); 
+  // childMessage = signal<string>('');
 
+  // receivedMessage(msg:string){
+  //   this.childMessage.set(msg);
+  // } 
 }
 
 
--- src/app/child/child.ts
+++ src/app/child/child.ts
@@ -1,18 +1,30 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output, signal, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-child',
-  imports: [],
+  imports: [FormsModule],
   template: `
     <div class="child">
       <h3>Child Component</h3>
-      <p>Message received from parent: {{message()}}</p>
-      <input type="text">
-      <button>Send Message!</button>
+      <!-- <p>Message received from parent: {{message()}}</p> -->
+       <p>Message received from parent: {{value()}}</p>
+      <input type="text" [(ngModel)]="value">
+      <!-- <input type="text" [(ngModel)]="childMessage" (input)="notify()"> -->
+      <!-- <button (click)="notify()">Send Message!</button> -->
+      <!-- <p>{{childMessage()}}</p> -->
     </div>
   `,
   styles: `.child{background-color: black ; color: white ; margin:2rem; padding:2rem}`
 })
 export class Child {
+  value = model<string>();
+  
   message = input<string>();
+  childMessage = signal<string>('');
+  Clicked = output<string>();
+  
+  notify(){
+    this.Clicked.emit(this.childMessage());
+  }
 }
